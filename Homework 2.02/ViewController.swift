3a0a448fab82f654c8e0641c90826d0e092e042b
Daniil Maiboroda <Dant2603@gmail.com>
2024-05-05 19:31:54 +0300

    Added logic to control traffic lights via button

--- Homework 2.02/ViewController.swift
+++ Homework 2.02/ViewController.swift
@@ -9,16 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightLabel: UIView!
+    @IBOutlet var yellowLightLabel: UIView!
+    @IBOutlet var greenLightLabel: UIView!
+    
+    @IBOutlet var lightSwitchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        for lightLabel in lightLabels {
-//            lightLabel.layer.cornerRadius = 50
-//            lightLabel.alpha = 1
-//        }
-        
+        redLightLabel.layer.cornerRadius = 50
+        yellowLightLabel.layer.cornerRadius = 50
+        greenLightLabel.layer.cornerRadius = 50
+        redLightLabel.alpha = 0.3
+        yellowLightLabel.alpha = 0.3
+        greenLightLabel.alpha = 0.3
+        lightSwitchButton.layer.cornerRadius = 10
+        lightSwitchButton.setTitle("START", for: .normal)
     }
-
-
+    
+    @IBAction func lightSwitchButtonDidTapped() {
+        switch lightSwitchButton.currentTitle {
+        case "START":
+            redLightLabel.alpha = 1.0
+            lightSwitchButton.setTitle("NEXT", for: .normal)
+        case "NEXT":
+            if redLightLabel.alpha == 1.0 {
+                redLightLabel.alpha = 0.3
+                yellowLightLabel.alpha = 1.0
+            } else if yellowLightLabel.alpha == 1.0 {
+                yellowLightLabel.alpha = 0.3
+                greenLightLabel.alpha = 1.0
+            } else if greenLightLabel.alpha == 1.0 {
+                greenLightLabel.alpha = 0.3
+                redLightLabel.alpha = 1.0
+            }
+        default:
+            break
+        }
+    }
 }
 
